e93fda3dee6dea82f5e72405760cf120260ffdd8
Suraj Bhardwaj <bhardwajsuraj1507@gmail.com>
2023-07-09 00:57:07 +0530

    create message function

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,54 +1,106 @@
 import React, { useEffect, useState } from 'react'
 import { COLLECTION_ID_MESSAGES, DATABASE_ID, databases } from '../appwriteConfig';
+import { ID } from 'appwrite'
+
 
 const Room = () => {
 
-  const [messages, setMessages] = useState([]);
+  const [messageBody, setMessageBody] = useState('')
+  const [messages, setMessages] = useState([])
+  // const { user } = useAuth()
+
+
 
   useEffect(() => {
     getMessages()
   }, [])
 
+
+  // to show the data form the data base
   const getMessages = async () => {
     const response = await databases.listDocuments(DATABASE_ID, COLLECTION_ID_MESSAGES)
     console.log(response)
     setMessages(response.documents)
   }
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const payload = {
+      body: messageBody
+    }
+
+    const response = await databases.createDocument(
+      DATABASE_ID,
+      COLLECTION_ID_MESSAGES,
+      ID.unique(),
+      payload
+    )
+
+    console.log("Created", response)
+
+    setMessageBody('')
+  }
+
+
 
 
   return (
     <main className='container'>
       <div className='room--container'>
-        {messages.map(message => (
-          <div key={message.$id} className="message--wrapper">
-            <div className="message--header">
-              <p>
-                {message?.username ? (
-                  <span> {message?.username}</span>
-                ) : (
-                  'Anonymous user'
-                )}
 
-                <small className="message-timestamp"> {new Date(message.$createdAt).toLocaleString()}</small>
-              </p>
-              <div className="message--body">
-                <span>{message.body}</span>
-              </div>
-              {/* {message.$permissions.includes(`delete(\"user:${user.$id}\")`) && (
+        <form id="message--form" onSubmit={handleSubmit}>
+          <div>
+            <textarea
+              required
+              maxLength="250"
+              placeholder="Say something..."
+              onChange={(e) => { setMessageBody(e.target.value) }}
+              value={messageBody}
+            ></textarea>
+          </div>
+
+          <div className="send-btn--wrapper">
+            <input
+              className="btn btn--secondary"
+              type="submit"
+              value="send"
+            />
+          </div>
+        </form>
+
+
+        <div>
+          {messages.map(message => (
+            <div key={message.$id} className="message--wrapper">
+              <div className="message--header">
+                <p>
+                  {message?.username ? (
+                    <span> {message?.username}</span>
+                  ) : (
+                    'Anonymous user'
+                  )}
+
+                  <small className="message-timestamp"> {new Date(message.$createdAt).toLocaleString()}</small>
+                </p>
+                <div className="message--body">
+                  <span>{message.body}</span>
+                </div>
+                {/* {message.$permissions.includes(`delete(\"user:${user.$id}\")`) && (
               <Trash2 className="delete--btn" onClick={() => { deleteMessage(message.$id) }} />
 
             )} */}
-            </div>
+              </div>
 
-            {/* <div className={"message--body" + (message.user_id === user.$id ? ' message--body--owner' : '')}>
+              {/* <div className={"message--body" + (message.user_id === user.$id ? ' message--body--owner' : '')}>
             <span>{message.body}</span>
 
           </div> */}
 
 
-          </div>
-        ))}
+            </div>
+          ))}
+        </div>
       </div>
     </main>
   )
