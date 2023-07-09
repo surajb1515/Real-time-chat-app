81161155cceb25a9e52286405028e6a0525708a4
Suraj Bhardwaj <bhardwajsuraj1507@gmail.com>
2023-07-09 15:09:13 +0530

    subscribe event

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { COLLECTION_ID_MESSAGES, DATABASE_ID, databases } from '../appwriteConfig';
+import client, { COLLECTION_ID_MESSAGES, DATABASE_ID, databases } from '../appwriteConfig';
 import { ID, Query } from 'appwrite'
+import { Trash2 } from 'react-feather'
+
+
+
 
 
 const Room = () => {
@@ -11,19 +15,49 @@ const Room = () => {
 
 
 
+
   useEffect(() => {
-    getMessages()
-  }, [])
+    getMessages();
+
+    // SUBSCRIBE TO SOME EVENTS
+    // SUBSCRIBING TO CREATE, UPDATA , DELETE METHODS
+
+    const unsubscribe = client.subscribe(`databases.${DATABASE_ID}.collections.${COLLECTION_ID_MESSAGES}.documents`, response => {
+
+      if (response.events.includes("databases.*.collections.*.documents.*.create")) {
+        console.log('A MESSAGE WAS CREATED')
+        setMessages(prevState => [response.payload, ...prevState])
+      }
+
+      if (response.events.includes("databases.*.collections.*.documents.*.delete")) {
+        console.log('A MESSAGE WAS DELETED!!!')
+        setMessages(prevState => prevState.filter(message => message.$id !== response.payload.$id))
+      }
+    });
+
+    console.log('unsubscribe:', unsubscribe)
 
 
-  // to show the data form the data base
+    //  CLEAN UP FUNCTION AS SUBSCRIBE IS CALLED TWICE 
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+
+
+
+
+
+  // TO POPULATE MESSAGES FORM THE DATABASE----->
+
   const getMessages = async () => {
     const response = await databases.listDocuments(
       DATABASE_ID,
       COLLECTION_ID_MESSAGES,
       [
-        Query.orderDesc('$createdAt'),
-        Query.limit(20)
+        Query.orderDesc('$createdAt'),    // for the desending order 
+        Query.limit(20)                 // to limit the response data 
       ]
     )
     console.log(response)
@@ -32,6 +66,11 @@ const Room = () => {
 
 
 
+
+
+
+  // TO SEND THE MESSAGE----------->
+
   const handleSubmit = async (e) => {
     e.preventDefault()
 
@@ -39,21 +78,29 @@ const Room = () => {
       body: messageBody
     }
 
+    // to create the message into our dataBase
     const response = await databases.createDocument(
       DATABASE_ID,
       COLLECTION_ID_MESSAGES,
-      ID.unique(),
+      ID.unique(),              // unique id to distinguish
       payload,
-
     )
 
     console.log("Created", response)
 
+    // to add the message into our setMessages array
     setMessages(prevState => [response, ...messages])
 
-    setMessageBody('')
+    setMessageBody('')      // to clear the input text area after sending the message
   }
 
+
+
+
+
+
+
+  // DELETE THE MESSAGE ---->
 
   const deleteMessage = async (message_id) => {
     await databases.deleteDocument(
@@ -63,6 +110,14 @@ const Room = () => {
     );
     setMessages(prevState => prevState.filter(message => message.$id !== message_id))
   }
+
+
+
+
+
+
+
+
 
 
 
@@ -109,10 +164,11 @@ const Room = () => {
                 <div className="message--body">
                   <span>{message.body}</span>
                 </div>
-                {message.$permissions.includes(`delete(\"user:${user.$id}\")`) && (
+                {/* {message.$permissions.includes(`delete(\"user:${user.$id}\")`) && (
                   <Trash2 className="delete--btn" onClick={() => { deleteMessage(message.$id) }} />
 
-                )}
+                )} */}
+                <Trash2 className="delete--btn" onClick={() => { deleteMessage(message.$id) }} />
               </div>
 
               {/* <div className={"message--body" + (message.user_id === user.$id ? ' message--body--owner' : '')}>
